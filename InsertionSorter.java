f651f28f5de4d72e92c5404c54340e86fd9a14ee
YeseulChoi0 <ychoi00@stuy.edu>
2019-05-06 20:14:10 -0400

    InsertionSorter.java

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -9,15 +9,37 @@ public class InsertionSorter extends Sorter {
     /**
       Construct an instance to process the user's data
      */
-    public InsertionSorter(  ArrayList< String> usersData) {
-        // your code here, if necessary
+    public InsertionSorter(ArrayList<String> usersData){
+      elements = usersData;
     }
-    
 
-    /**
-      sort the user's data, implementing insertion sort
-     */
-    public void mySort() {
-        // your code here
+    public void mySort(){
+      for (int indexOfInsertion = 1; indexOfInsertion < elements.size(); indexOfInsertion++){
+        insert1(indexOfInsertion);
+        System.out.println("list: " + elements);
+      }
+    }
+
+    public void insert1(int alreadyInserted){
+      String valueToInsert = elements.get(alreadyInserted);
+      for (int indexToCompare = alreadyInserted-1; indexToCompare >= 0; indexToCompare--){
+        if (valueToInsert.compareTo(elements.get(indexToCompare)) < 0){
+          shift(indexToCompare);
+          if (indexToCompare == 0){
+            dropValue(0, valueToInsert);
+            return;
+          }
+        }else{
+          dropValue(indexToCompare + 1, valueToInsert);
+        }
+      }
+    }
+
+    public void dropValue(int insertIndex, String valueToInsert){
+      elements.set(insertIndex, valueToInsert);
+    }
+
+    public void shift(int shiftingIndex){
+      elements.set(shiftingIndex+1, elements.get(shiftingIndex));
     }
 }
